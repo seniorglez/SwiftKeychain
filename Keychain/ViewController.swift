f1907d026b2df9a5da6f15ee99bd21d727ab10e1
Diego Domínguez González <ddominguez@solidgear.es>
2020-04-22 22:21:46 +0200

    UI implemented

--- Keychain/ViewController.swift
+++ Keychain/ViewController.swift
@@ -10,12 +10,68 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let buttonLogIn: UIButton = UIButton()
+    let userTextField: UITextField = UITextField()
+    let passwordTextField: UITextField = UITextField()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.accessibilityIdentifier = "startView"
         view.backgroundColor = .red
-        // Do any additional setup after loading the view.
+        createUIElements()
     }
+    
+    func createUIElements() {
+        configureUserTextField()
+        configurePasswordTextField()
+        configureButton()
+    }
+    
+    func configureUserTextField() {
+        view.addSubview(userTextField)
+        userTextField.backgroundColor = .white
+        userTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            userTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 5),
+            userTextField.leftAnchor.constraint(equalTo:view.leftAnchor),
+            userTextField.rightAnchor.constraint(equalTo:view.rightAnchor),
+            userTextField.heightAnchor.constraint(equalToConstant: view.frame.height/18)
+        ])
 
-
+    }
+    
+    func configurePasswordTextField() {
+        view.addSubview(passwordTextField)
+        passwordTextField.backgroundColor = .white
+        passwordTextField.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            passwordTextField.topAnchor.constraint(equalTo: userTextField.bottomAnchor, constant: 5),
+            passwordTextField.leftAnchor.constraint(equalTo:view.leftAnchor),
+            passwordTextField.rightAnchor.constraint(equalTo:view.rightAnchor),
+            passwordTextField.heightAnchor.constraint(equalToConstant: view.frame.height/18)
+        ])
+    }
+    
+    func configureButton() {
+        buttonLogIn.setTitle("Log In", for: .normal)
+        buttonLogIn.backgroundColor = .orange
+        buttonLogIn.layer.cornerRadius = 5
+        buttonLogIn.addTarget(self, action: #selector(storeOnKeychain), for: .touchUpInside)
+        
+        view.addSubview(buttonLogIn)
+        buttonLogIn.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            buttonLogIn.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20),
+            buttonLogIn.heightAnchor.constraint(equalToConstant: 35),
+            buttonLogIn.widthAnchor.constraint(equalToConstant: view.frame.width/3),
+            buttonLogIn.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
+    @objc func storeOnKeychain() {
+        print("storing on keychain")
+    }
 }
-
---
+++ Keychain/ViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewModel.swift
+//  Keychain
+//
+//  Created by Diego Domínguez González on 22/04/2020.
+//  Copyright © 2020 Diego Domínguez González. All rights reserved.
+//
+
+import Foundation
+
+class ViewModel {
+    
+}
