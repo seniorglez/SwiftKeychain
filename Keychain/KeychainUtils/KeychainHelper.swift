3b2c238cb94021f0118b9d4f7801a329f2bfca5c
Diego Domínguez González <ddominguez@solidgear.es>
2020-04-28 12:51:09 +0200

    added func to update and remove credentials from keychain

--- Keychain/KeychainUtils/KeychainHelper.swift
+++ Keychain/KeychainUtils/KeychainHelper.swift
@@ -7,6 +7,7 @@
 //
 // apple´s article about this: https://developer.apple.com/documentation/security/keychain_services/keychain_items/adding_a_password_to_the_keychain
 //Keychain error codes meaning: https://www.oreilly.com/library/view/ios-components-and/9780133086898/ch18lev2sec7.html
+//Updating and deleting KeyChain items: https://developer.apple.com/documentation/security/keychain_services/keychain_items/updating_and_deleting_keychain_items
 import Foundation
 
 class KeychainHelper {
@@ -35,14 +36,33 @@ class KeychainHelper {
         guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }    }
 }
 
-    func clearKeychain() {
-        let secItemClasses =  [kSecClassGenericPassword, kSecClassInternetPassword, kSecClassCertificate, kSecClassKey, kSecClassIdentity]
-        for itemClass in secItemClasses {
-            let spec: NSDictionary = [kSecClass: itemClass]
-            SecItemDelete(spec)
-      }
+func clearKeychain() {
+    let secItemClasses =  [kSecClassGenericPassword, kSecClassInternetPassword, kSecClassCertificate, kSecClassKey, kSecClassIdentity]
+    for itemClass in secItemClasses {
+        let spec: NSDictionary = [kSecClass: itemClass]
+        SecItemDelete(spec)
+    }
+    
+    func updateKeychain(credentials: Credentials, URL server: String) throws {
+        let query: [String: Any] = [kSecClass as String: kSecClassInternetPassword,
+                                    kSecAttrServer as String: server]
+        let account = credentials.username
+        let password = credentials.password.data(using: String.Encoding.utf8)!
+        let attributes: [String: Any] = [kSecAttrAccount as String: account,
+                                         kSecValueData as String: password]
+        let status = SecItemUpdate(query as CFDictionary, attributes as CFDictionary)
+        guard status != errSecItemNotFound else { throw KeychainHelper.KeychainError.noPassword }
+        guard status == errSecSuccess else { throw KeychainHelper.KeychainError.unhandledError(status: status) }
+    }
+    
+    func removeFromKeychain(URL server: String) throws {
+        let query: [String: Any] = [kSecClass as String: kSecClassInternetPassword,
+        kSecAttrServer as String: server]
+        let status = SecItemDelete(query as CFDictionary)
+        guard status == errSecSuccess || status == errSecItemNotFound else { throw KeychainHelper.KeychainError.unhandledError(status: status) }
+    }
 }
-
+//MARK - Extensions
 //Usually I add this extension on the Network group
 extension URL {
     static let serverURL: URL = URL(string : "https://stackoverflow.com")!
